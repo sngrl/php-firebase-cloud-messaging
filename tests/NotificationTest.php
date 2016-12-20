2d8991884f74cca712bfcfd12c38ad870526f749
Nandor Huszar <Nandor Huszar>
2016-12-20 12:48:25 +0000

    Adding content_available option test

--- tests/NotificationTest.php
+++ tests/NotificationTest.php
@@ -29,4 +29,10 @@ class NotificationTest extends PhpFirebaseCloudMessagingTestCase
         $this->fixture->setIcon('name');
         $this->assertEquals(array('title' => 'foo', 'body' =>'bar', 'icon' => 'name'), $this->fixture->jsonSerialize());
     }
+
+    public function testJsonSerializeWithContentAvailable()
+    {
+        $this->fixture->setContentAvailable(true);
+        $this->assertEquals(array('title' => 'foo', 'body' =>'bar', 'content_available' => true), $this->fixture->jsonSerialize());
+    }
 }
